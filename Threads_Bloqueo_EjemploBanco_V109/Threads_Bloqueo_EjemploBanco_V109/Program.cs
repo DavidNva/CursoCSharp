391db23c5546e34ba1d553c6616b3e56f182a5fe
David Nava <david.nava.garcia4@gmail.com>
2022-08-08 20:32:50 -0500

    Hilos. Ejemplo 2. Sincronizacion y bloqueo. Video 110

---
+++ Threads_Bloqueo_EjemploBanco_V109/Threads_Bloqueo_EjemploBanco_V109/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+namespace Threads_Bloqueo_EjemploBanco_V109
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola");
+            CuentaBancaria CuentaFamilia = new CuentaBancaria(2000);//En inicio la cuenta tendra 2000 pesos de saldo
+            Thread[] hiloPersonas = new Thread[15];
+            for(int i = 0; i< 15; i++)
+            {
+                //La instancia thread no permite metodos con parametro, por eso creamos otro para acceder a la informacion
+                //de retiro y su saldo con otro metodo sin parametros
+                Thread t = new Thread(CuentaFamilia.VamosRetirarEfectivo);
+                t.Name = i.ToString();//Se le da un nombre a cada hilo, comenzando desde el cero
+                hiloPersonas[i] = t;//En cada posicion del array hilosPersonas, insertamos la nueva instancia creada (hilo creado)
+            }
+            for (int i = 0; i < 15; i++)
+            {
+                hiloPersonas[i].Start();//Comenzamos esos 15 threads creados, con sus intrucciones especificados
+                                        //en este caso inican con elmetodo de vamos a retirar efectivo
+                //Sincronizacion de hilos con join();
+                hiloPersonas[i].Join();//De esta forma primero ejecuta el hilo 0 y con cuatro retiros de 500 el hilo vacia la cuenta de 2000
+                //pues cuando se ejecuten los demas hilos, la cuenta estará en 0
+                //NOTA, si se desea hacer pruebas de como funciona bloqueando los hilos: Entonces comentar esta ultima linea
+                //y descomentar el metodo look de las lineas de abajo
+            }
+        }
+    }
+
+    class CuentaBancaria
+    {
+        private Object bloqueaSaldoPositivo =  new object();//Como parametro del bloque de hilos
+        double Saldo { get; set; }
+        public CuentaBancaria(double Saldo)
+        {
+            this.Saldo = Saldo;
+        }
+        public double RetirarEfectivo(double cantidad)
+        {
+            if ((Saldo - cantidad) < 0) { //La instruccion nos muestra el thread desde el que se ejecuta este metodo(recordando que tenemos 15 thread, hilos)
+                Console.WriteLine($"Lo sentimos solo quda {Saldo} persos en la cuenta: Hilo: {Thread.CurrentThread.Name}");
+                return Saldo;
+            }
+
+            //Este trozo de codigo es el que importa al acceder a un thread, porque si algun hilo esta accediendo, pues bloqueamos los demas hilos
+            //usando el metodo look
+           // lock (bloqueaSaldoPositivo) {
+           //Si solo sincronizamos los hilos, entonces primero hará los procesos del metodo 1, o el que haya entrado y cuando termine seguira con los otros
+                if (Saldo >= cantidad)
+                {
+                    Console.WriteLine($"Retiro de {cantidad}. Queda {Saldo - cantidad} de saldo en la cuenta. Hilo: {Thread.CurrentThread.Name}");
+                    return Saldo -= cantidad; //
+                                              //return Saldo  = Saldo - cantidad
+                }
+                return Saldo;//Dependiendo de a cual if entro, si al primero no retorna el mismo saldo porque en realidad no se hizo ninguna operacon
+                             //pero en el segundo if, si lo hace, por eso se le da un nuevo valor a saldo
+            //}//De esta forma, si un hilo esta accediendo a este codigo (el cual encontró que la cuenta aun tiene dinero) los demas no podran acceder
+            //y lo que dirá será lo de no queda saldo en la cuenta//Obviamente es como ejemplo. En teoria si hay dinero, pues se puede seguir sacando dinero
+        }
+        public void VamosRetirarEfectivo()
+        {
+            Console.WriteLine($"Está sacando dinero del Hilo: {Thread.CurrentThread.Name}");
+            for (int i = 0; i < 4; i++) RetirarEfectivo(500);//Para reperit cuatro veces el retiro, y de forma visual veamos la distorcion de hilo
+            //al interntar sacar dinero, que se supone ya no existe al estar retirado por un hilo. Y ademas de que en teoria no deberia permitir que el saldo tenga valores negativo
+
+        }
+    }
+}
--- Threads_V108/Threads_V108/Program.cs
+++ Threads_V108/Threads_V108/Program.cs
@@ -8,12 +8,10 @@ namespace Threads_V108
             //Para crear un hilo se usa la clase thread
             Thread t = new Thread(MetodoSaludo);//En sus argumentos se le indica la accion a realizar por ese segundo hilo al principal
             t.Start();//Este metodo pone en ejecucion ese hilo
+            t.Join();
             //Con esto, nos damos cuenta que se esta realizando con el hecho de que imprime los valores de manera aleatoria por decirlo de alguna forma
             //Por que como son dos hilos, se van ejecutando y es el procesador el que les da atencion o prioridad a uno, luego a otro, etc.
-            Thread t2 = new Thread(MetodoSaludo);//En sus argumentos se le indica la accion a realizar por ese segundo hilo al principal
-            t2.Start();//POdemos indicar otro hilo, que ejecuta el mismo metodos
-            //Al ejecutar nos darmeos cuenta del funcionamiento de esto
-            //Ademas si depuramos podremos observar los subprocesos
+            
             Console.WriteLine("Ejemplo trabajar con hilos");
             Console.WriteLine("Imprimiendo desde el thread (o hilo ) 1");
             Thread.Sleep(500);//Duerme el hilo, medio segundo (500 milisegundos)
@@ -27,10 +25,24 @@ namespace Threads_V108
             //La depuracion no mantiene en un punto en concreto y nos muestras los subprocesos que tenemos
             //o los hilos como tal
             //MetodoSaludo();
+
+            Thread t2 = new Thread(MetodoSaludo);//En sus argumentos se le indica la accion a realizar por ese segundo hilo al principal
+            t2.Start();//POdemos indicar otro hilo, que ejecuta el mismo metodos
+            //Al ejecutar nos darmeos cuenta del funcionamiento de esto
+            //Ademas si depuramos podremos observar los subprocesos
+            t2.Join();//Metodo para sincronizar los hilos e indicar cuando termine uno, comienece otro y no como en los primeros ejemplos donde
+            //el procesador decidia cuando hacer caso a uno y cuando a otro
+
+
+            //Ahora si va hilo por hilo
+            //Primero el primer hilo t que inidicamos
+            //luego continua con el principal, donde tenemos en teoria el hilo uno{
+            //y luego el t2 (seria el tercer hilo, o segundo subhilo) 
+            //Al imprimir veremeos primero 5 veces el dos, luego 5 veces el 1 y por ultimo 5 veces el 2, sincronizadamente
         }
         static void MetodoSaludo()
         {
-            Console.WriteLine("Imprimiendo desde el thread (o hilo ) 1");
+            Console.WriteLine("Imprimiendo desde el thread (o hilo ) 2");
             Thread.Sleep(500);//Duerme el hilo, medio segundo (500 milisegundos)
             Console.WriteLine("Imprimiendo desde el thread (o hilo ) 2");
             Thread.Sleep(500);
